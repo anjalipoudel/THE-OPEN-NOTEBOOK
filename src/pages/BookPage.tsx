d6f2cf813be1e1be2bbb82672e448cf740987fd6
anjalipoudel <sabnom77poudel@gmail.com>
2023-03-03 16:57:47 +0545

    init

--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,24 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function
-    () {
+export default function BookPage() {
     return (
-        <div className='flex py-10'>
-            <div className='flex flex-col shadow-xl rounded-xl h-80 m-5 leading-4'>
+        <div className='flex gap-32 py-10'>
+            <div className='flex flex-col shadow-xl rounded-xl   leading-4'>
                 <div className='bg-gray-50' >
                     <Image
-                        src="/assets/img/simulation.jpg"
+                        src="/assets/img/bookpage.jpg"
                         alt="wishlist Section"
-                        width={200}
-                        height={80}
+                        width={350}
+                        height={50}
                         className="object-cover flex items-center p-3"
                         quality={100}
                     />
                 </div>
             </div>
-            <div>
-                <div> SIMULATION & MODELING</div>
+            <div className='bg-red w-auto h-auto text-xl font-medium text-gray-500 leading-10'>
+                <div className='text-[#7de3bb] text-3xl'> SIMULATION & MODELING</div>
                 <div>Price:
                     RS. 395</div>
 
@@ -39,8 +38,8 @@ export default function
                 <div> Version:
                     English
                 </div>
-                <div className='justify-center flex'>
-                    <input type='button' value={`Buy Now`} className='cursor-pointer bg-[#c93b27]  p-3 rounded text-white hover:bg-blue-600' />
+                <div className=' flex'>
+                    <input type='button' value={`Buy Now`} className='cursor-pointer bg-[#7de3bb]  p-2 rounded text-white hover:bg-blue-600' />
                 </div>
             </div>
         </div>
